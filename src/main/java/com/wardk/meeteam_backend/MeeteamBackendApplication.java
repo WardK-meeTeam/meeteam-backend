4ad8a3fdf4c955d761db7cd3b5ecab9ca5c7b8db
discipline24 <ejsky33@naver.com>
2025-05-17 13:26:34 +0900

    Initial commit

---
+++ src/main/java/com/wardk/meeteam_backend/MeeteamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wardk.meeteam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeteamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeteamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wardk/meeteam_backend/MeeteamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wardk.meeteam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeteamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
