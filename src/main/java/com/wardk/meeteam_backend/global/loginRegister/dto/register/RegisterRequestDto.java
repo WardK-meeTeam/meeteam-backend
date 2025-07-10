b85867296913cd205b9409bd4d8abb28cf7a8e2d
heeun98 <pygphu98@gmail.com>
2025-07-10 16:21:12 +0900

    Swagger 필드설명

--- src/main/java/com/wardk/meeteam_backend/global/loginRegister/dto/register/RegisterRequestDto.java
+++ src/main/java/com/wardk/meeteam_backend/global/loginRegister/dto/register/RegisterRequestDto.java
@@ -2,6 +2,7 @@ package com.wardk.meeteam_backend.global.loginRegister.dto.register;
 
 
 import com.wardk.meeteam_backend.domain.member.entity.JobType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -12,18 +13,23 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 public class RegisterRequestDto {
 
+    @Schema(description = "이름", example = "홍길동")
     @NotEmpty
     private String name;
 
+    @Schema(description = "이메일", example = "meeteam@naver.com")
     @NotEmpty
     private String email;
 
+    @Schema(description = "비밀번호", example = "qwer1234")
     @NotEmpty
     private String password;
 
+    @Schema(description = "직무선택", example = "BACKEND")
     @NotNull
     private JobType jobType;
 
+    @Schema(description = "프로필사진 업로드")
     private MultipartFile file;
 }
 
