f613a23ddc9cf34ce1bb15d4d8b4b9e183a7ef71
heeun98 <pygphu98@gmail.com>
2025-08-19 23:23:27 +0900

    test2

--- src/main/java/com/wardk/meeteam_backend/domain/member/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/com/wardk/meeteam_backend/domain/member/repository/MemberRepositoryCustomImpl.java
@@ -26,14 +26,11 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
     @Override
     public Member getProfile(Long memberId) {
 
-
         Member findMember = queryFactory
                 .select(member)
                 .from(member)
                 .where(member.id.eq(memberId))
                 .fetchOne();
-
-
 
         if (findMember == null) {
             throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
