e2099d3fe1d0f28e4f0544ab11b5c0a4d736b871
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-08-20 01:52:14 +0900

    refactor : 빌드 후 오류 수정

--- src/main/java/com/wardk/meeteam_backend/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/wardk/meeteam_backend/domain/chat/repository/ChatMessageRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List; import java.util.UUID;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
-  List<ChatMessage> findByThreadIdOrderByCreatedAtAsc(UUID threadId);
+  //List<ChatMessage> findByThreadIdOrderByCreatedAtAsc(UUID threadId);
 }
