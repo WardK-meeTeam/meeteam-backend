50ada7dba8231ac5b9cabe0a3ed3d76421c53703
heeun98 <pygphu98@gmail.com>
2025-08-25 18:07:58 +0900

    refactor:PROJECT_APPLY_응답_수정

--- src/main/java/com/wardk/meeteam_backend/web/notification/dto/NewApplicantPayload.java
+++ src/main/java/com/wardk/meeteam_backend/web/notification/dto/NewApplicantPayload.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class NewApplicantPayload { // 내 프로젝트에 누가 지원함(프로젝트 소유자에게 가는 카드)
 
+    Long receiverId; // 팀장 ID
     Long applicantId; // 참여자
     String applicantName;
     String projectName;
