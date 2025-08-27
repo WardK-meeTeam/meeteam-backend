0f99149fedb10619f20f1fa604558fa6cafdf70e
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-08-27 13:16:47 +0900

    refactor : 자잘한 오류 수정, 검증 로직 추가 등 리뷰 반영

--- src/main/java/com/wardk/meeteam_backend/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/wardk/meeteam_backend/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,10 @@
 package com.wardk.meeteam_backend.domain.chat.repository;
 
 import com.wardk.meeteam_backend.domain.chat.entity.ChatMessage;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List; import java.util.UUID;
 
