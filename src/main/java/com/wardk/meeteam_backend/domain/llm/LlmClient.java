ba01518f4afd560ff809c549c3072ebc73118bc2
나의찬 <cab07036@naver.com>
2025-09-04 03:16:17 +0900

    remove: 기존 파일 삭제

--- src/main/java/com/wardk/meeteam_backend/domain/llm/LlmClient.java
+++
@@ -1,9 +0,0 @@
-package com.wardk.meeteam_backend.domain.llm;
-
-
-import java.util.function.Consumer;
-
-public interface LlmClient {
-  void stream(LlmRequest request, Consumer<String> onChunk, Consumer<LlmUsage> onDone);
-}
-
--- src/main/java/com/wardk/meeteam_backend/domain/llm/LlmOrchestrator.java
+++
@@ -1,9 +0,0 @@
-package com.wardk.meeteam_backend.domain.llm;
-
-import java.util.UUID;
-
-public interface LlmOrchestrator {
-  void generateInitialReview(UUID threadId, UUID prId, String prSummary);
-  void generateFollowup(UUID threadId);
-  void handleToolCall(UUID threadId, Object toolCall); // TODO: ToolCall 타입 후속 정의
-}
