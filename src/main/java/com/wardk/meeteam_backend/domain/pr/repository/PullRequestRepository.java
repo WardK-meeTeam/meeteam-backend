24c083e5b95a3f25fc545bedc890880a11ca94ef
나의찬 <cab07036@naver.com>
2025-09-30 04:11:58 +0900

    feat: 특정 레포 내의 PR 데이터 조회 API 추가 + JWT 토큰 검증 로직 추가

--- src/main/java/com/wardk/meeteam_backend/domain/pr/repository/PullRequestRepository.java
+++ src/main/java/com/wardk/meeteam_backend/domain/pr/repository/PullRequestRepository.java
@@ -1,7 +1,6 @@
 package com.wardk.meeteam_backend.domain.pr.repository;
 
 import com.wardk.meeteam_backend.domain.pr.entity.PullRequest;
-import com.wardk.meeteam_backend.domain.webhook.entity.WebhookDelivery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,22 +10,25 @@ import java.util.Optional;
 
 public interface PullRequestRepository extends JpaRepository<PullRequest, Long> {
 
-  @Query(
-          """
-            SELECT pr
-            FROM PullRequest pr
-            JOIN FETCH pr.projectRepo repo
-            LEFT JOIN FETCH pr.files
-            WHERE repo.id = :projectRepoId AND pr.prNumber = :prNumber
-          """
-  )
-  Optional<PullRequest> findByProjectRepoIdAndPrNumber(Long projectRepoId, int prNumber);
+    @Query(
+            """
+                      SELECT pr
+                      FROM PullRequest pr
+                      JOIN FETCH pr.projectRepo repo
+                      LEFT JOIN FETCH pr.files
+                      WHERE repo.id = :projectRepoId AND pr.prNumber = :prNumber
+                    """
+    )
+    Optional<PullRequest> findByProjectRepoIdAndPrNumber(Long projectRepoId, int prNumber);
 
-  @Query("SELECT pr FROM PullRequest pr JOIN FETCH pr.projectRepo repo LEFT JOIN FETCH pr.files WHERE repo.repoFullName = :repoFullName AND pr.prNumber = :prNumber")
-  Optional<PullRequest> findWithFiles(String repoFullName, int prNumber);
+    @Query("SELECT pr FROM PullRequest pr JOIN FETCH pr.projectRepo repo LEFT JOIN FETCH pr.files WHERE repo.repoFullName = :repoFullName AND pr.prNumber = :prNumber")
+    Optional<PullRequest> findWithFiles(String repoFullName, int prNumber);
 
-  @Query("SELECT pr FROM PullRequest pr JOIN FETCH pr.projectRepo repo LEFT JOIN FETCH pr.files WHERE repo.project.id = :projectId AND repo.project.isDeleted = false")
-  List<PullRequest> findAllByProjectIdWithFiles(Long projectId);
+    @Query("SELECT pr FROM PullRequest pr JOIN FETCH pr.projectRepo repo LEFT JOIN FETCH pr.files WHERE repo.project.id = :projectId AND repo.project.isDeleted = false")
+    List<PullRequest> findAllByProjectIdWithFiles(Long projectId);
 
-  Optional<PullRequest> findByProjectRepoRepoFullNameAndPrNumber(String repoName, int prNumber);
+    Optional<PullRequest> findByProjectRepoRepoFullNameAndPrNumber(String repoName, int prNumber);
+
+    @Query("SELECT pr FROM PullRequest pr JOIN FETCH pr.projectRepo repo LEFT JOIN FETCH pr.files WHERE repo.repoFullName = :repoFullName")
+    List<PullRequest> findAllByRepoFullNameWithFiles(String repoFullName);
 }
--- src/main/java/com/wardk/meeteam_backend/web/pr/controller/PullRequestController.java
+++ src/main/java/com/wardk/meeteam_backend/web/pr/controller/PullRequestController.java
@@ -3,11 +3,13 @@ package com.wardk.meeteam_backend.web.pr.controller;
 import com.wardk.meeteam_backend.domain.pr.entity.PullRequest;
 import com.wardk.meeteam_backend.domain.pr.repository.PullRequestRepository;
 import com.wardk.meeteam_backend.domain.pr.service.PullRequestService;
+import com.wardk.meeteam_backend.global.auth.dto.CustomSecurityUserDetails;
 import com.wardk.meeteam_backend.global.response.SuccessResponse;
 import com.wardk.meeteam_backend.web.pr.dto.PullRequestResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,7 +34,8 @@ public class PullRequestController {
     public SuccessResponse<PullRequestResponse> get(
             @PathVariable String owner,
             @PathVariable String repo,
-            @PathVariable int prNumber) {
+            @PathVariable int prNumber,
+            @AuthenticationPrincipal CustomSecurityUserDetails userDetails) {
         String repoFullName = owner + "/" + repo;
         PullRequestResponse response = prService.getPullRequest(repoFullName, prNumber);
 
@@ -46,11 +49,29 @@ public class PullRequestController {
      */
     @Operation(summary = "특정 프로젝트 내 모든 PR 조회")
     @GetMapping("/{projectId}")
-    public SuccessResponse<List<PullRequestResponse>> getAllPullRequests(@PathVariable Long projectId) {
+    public SuccessResponse<List<PullRequestResponse>> getAllPullRequests(
+            @PathVariable Long projectId,
+            @AuthenticationPrincipal CustomSecurityUserDetails userDetails) {
 
         List<PullRequestResponse> responses = prService.getAllPullRequests(projectId);
 
         return SuccessResponse.onSuccess(responses);
     }
+
+    /**
+     *  특정 레포 내의 모든 PR 조회
+     */
+    @Operation(summary = "특정 레포 내의 모든 PR 조회")
+    @GetMapping("/{owner}/{repo}")
+    public SuccessResponse<List<PullRequestResponse>> getAllPullRequestsInRepo(
+            @PathVariable String owner,
+            @PathVariable String repo,
+            @AuthenticationPrincipal CustomSecurityUserDetails userDetails) {
+        String repoFullName = owner + "/" + repo;
+        List<PullRequestResponse> responses = prService.getAllPullRequestsInRepo(repoFullName);
+
+        return SuccessResponse.onSuccess(responses);
+    }
+
     // TODO: 필요 시 보강 조회/관리용 엔드포인트 추가
 }
