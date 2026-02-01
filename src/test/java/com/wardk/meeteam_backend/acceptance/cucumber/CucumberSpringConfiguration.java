f277e2100310e903bcdae1949eed1ca11dc5b50c
heeun98 <phu98@naver.com>
2026-02-02 01:23:20 +0900

    test: 훅에서 S3 페이크 서버 리셋

--- src/test/java/com/wardk/meeteam_backend/acceptance/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/wardk/meeteam_backend/acceptance/cucumber/CucumberSpringConfiguration.java
@@ -1,12 +1,15 @@
 package com.wardk.meeteam_backend.acceptance.cucumber;
 
 import com.wardk.meeteam_backend.acceptance.common.DatabaseCleaner;
+import com.wardk.meeteam_backend.acceptance.cucumber.support.FakeS3Server;
+import com.wardk.meeteam_backend.acceptance.cucumber.support.TestS3Config;
 import io.cucumber.java.Before;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,6 +23,7 @@ import javax.sql.DataSource;
 @CucumberContextConfiguration
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestS3Config.class)
 public class CucumberSpringConfiguration {
 
     @LocalServerPort
@@ -31,10 +35,14 @@ public class CucumberSpringConfiguration {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private FakeS3Server fakeS3Server;
+
     @Before
     public void setUp() {
         RestAssured.port = port;
         databaseCleaner.execute();
+        fakeS3Server.reset();
 
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("data.sql"));
