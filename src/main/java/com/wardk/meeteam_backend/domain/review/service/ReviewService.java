3000d4024a7e7205fff4ac4c283a05767f269439
heeun98 <phu98@naver.com>
2026-02-12 21:48:56 +0900

    chore: 리뷰 도메인 관련 코드 제거

--- src/main/java/com/wardk/meeteam_backend/domain/review/service/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package com.wardk.meeteam_backend.domain.review.service;
-
-import com.wardk.meeteam_backend.web.review.dto.request.ReviewRequest;
-import com.wardk.meeteam_backend.web.review.dto.response.ReviewResponse;
-
-public interface ReviewService {
-
-    public ReviewResponse review(ReviewRequest request, Long reviewerId);
-}
