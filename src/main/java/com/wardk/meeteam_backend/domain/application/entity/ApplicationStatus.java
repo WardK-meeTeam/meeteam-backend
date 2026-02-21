756f8e2ac3cfb4b8ee824c1b2b7e320bc6e247d1
heeun98 <phu98@naver.com>
2026-02-21 17:20:19 +0900

    refactor: 지원서 상태 ENUM 클래스 수정
    - displayName 필드 추가

--- src/main/java/com/wardk/meeteam_backend/domain/application/entity/ApplicationStatus.java
+++ src/main/java/com/wardk/meeteam_backend/domain/application/entity/ApplicationStatus.java
@@ -1,7 +1,18 @@
 package com.wardk.meeteam_backend.domain.application.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 프로젝트 지원 상태 Enum.
+ */
+@Getter
+@RequiredArgsConstructor
 public enum ApplicationStatus {
-    PENDING, // 대기 중
-    ACCEPTED, // 수락
-    REJECTED // 거절
+
+    PENDING("대기중"),
+    ACCEPTED("승인됨"),
+    REJECTED("거절됨");
+
+    private final String displayName;
 }
